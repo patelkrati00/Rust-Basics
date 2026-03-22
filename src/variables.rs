90a238e104ed4a6fec1d316dd69090ee84ab5266
Krati Patel <patelkrati00@gmail.com>
2026-03-22 18:06:35 +0530

    Strings in rust

--- src/variables.rs
+++ src/variables.rs
@@ -15,12 +15,33 @@ pub fn show() {
 
     if is_female {
         println!("female")
-    }else{
+    } else {
         print!("not a female")
     }
 
-    if is_above_18 && is_female{
-        print!("legal female")
+    if is_above_18 && is_female {
+        println!("legal female")
     }
+
+    // String is a growable, heap-allocated type.
+    // Its size can change at runtime, unlike fixed-size arrays.
+    // Because of this, memory is managed dynamically.
+
+    // one way to write strings
+    let greeting: String = String::from("hello World");
+    println!("{}", greeting);
+
+    //get nth char
+    let char1: Option<char> = greeting.chars().nth(1);
+
+    // println!("{}",char1.unwrap()); //unwrap is tells compiler that it is okay with the runtime error if it happens(it'll throw error if index is larger than a string) if its valid index toh it'll give the required char
+
+    //another way
+    // pattern matching
+
+    match char1 {
+        Some(c) => println!("{}", c),
+        None => println!("No character at index 1000")
+    };
 
 }
